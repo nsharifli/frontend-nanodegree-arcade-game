d6f02e46fdb9ce9c526337cc6836bc11eec578ce
Nikki Sharifli <nikki.sharifli@gmail.com>
2018-11-12 11:50:55 -0500

    Convert basic enemy function  to ES6 class syntax

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy {
+  constructor() {
     this.sprite = 'images/enemy-bug.png';
-};
+    this.x = 0;
+    this.y = 0;
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  // Draw the enemy on the screen, required method for game
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 };
 
 // Now write your own player class
